ef1ea9b81cd0e335474be842c8aae901109b60dc
Eli Golding <egolding@qualitybath.com>
2018-02-15 17:55:29 -0500

    Optionally set status code in RedirectException. Useful for SSR.

--- src/RedirectException.js
+++ src/RedirectException.js
@@ -1,6 +1,13 @@
 // This isn't really an error.
 export default class RedirectException {
-  constructor(location) {
-    this.location = location;
+  // Optionally set status code. Useful for SSR.
+  constructor(status, location) {
+    if (location) {
+      this.status = status;
+      this.location = location;
+    } else {
+      this.status = 302;
+      this.location = status;
+    }
   }
 }
